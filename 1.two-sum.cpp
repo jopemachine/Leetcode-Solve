7c6c699620b37d6d51fffa8fc71594d1f3b5602c
jopemachine <jopemachine@naver.com>
2020-06-28 18:15:57 -0700

    Create 1.two-sum.cpp

---
+++ 1.two-sum.cpp
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=1 lang=cpp
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+        vector<int> res;
+
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if(nums[i] + nums[j] == target) {
+                    res.push_back(i);
+                    res.push_back(j);
+                    return res;
+                }
+            }
+        }
+
+        return res;
+    }
+};
+// @lc code=end
+
