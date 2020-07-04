eceb42dec2b7c4b839ee1c2724debd8d07b8c858
jopemachine <jopemachine@naver.com>
2020-07-04 16:32:59 -0700

    Update 1.two-sum.cpp

--- 1.two-sum.cpp
+++ 1.two-sum.cpp
@@ -5,9 +5,47 @@
  */
 
 // @lc code=start
+
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
 class Solution {
 public:
+
     vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> res;
+        unordered_map<int, int> map;
+
+        for (int i = 0; i < nums.size(); i++) {
+            // number, index
+            map.insert({ nums[i], i });
+        }
+        
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+
+            // find complement among the key
+            if(map.count(complement)) {
+                // if below key exist, two sum condition satisfy
+                if(i != map.at(complement)) {
+                    res = { i, map.at(complement) };
+                    return res;
+                }
+            }
+
+            // inefficient search
+
+            // for(unordered_map<int, int>::iterator it = map.begin(); it != map.end(); ++it) {
+            //  ....
+            // }
+        }
+        return res;
+    }
+
+    vector<int> twoSum_BruteForce(vector<int>& nums, int target) {
         
         vector<int> res;
 
